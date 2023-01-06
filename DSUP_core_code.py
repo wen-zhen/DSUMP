56d18b1074f075553b40b1ebe1fe9dc5ad91de05
wen-zhen <43161320+wen-zhen@users.noreply.github.com>
2023-01-06 17:17:54 +0800

    Delete DSUP_core_code.py

--- DSUP_core_code.py
+++
@@ -1,8 +0,0 @@
-import math
-import h5py
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.stats import norm
-from math import radians, cos, sin, asin, sqrt
-import surface.setting_data.setting as st
-import get_max_z as gmz
