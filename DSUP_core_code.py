938890216332778c44b6a4f66c1baaed951cb44d
wen-zhen <1782648366@qq.com>
2023-01-06 16:22:50 +0800

    first commit

---
+++ DSUP_core_code.py
@@ -0,0 +1,8 @@
+import math
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+from math import radians, cos, sin, asin, sqrt
+import surface.setting_data.setting as st
+import get_max_z as gmz
